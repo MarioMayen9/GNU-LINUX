f49f3a9a09384855c2bf99c4c0b14bf80d91198d
MarioMayen9 <00220618@uca.edu.sv>
2024-11-25 01:31:38 -0600

    adding pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders all cards', () => {
+  // Renderiza la aplicación
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  // Verifica que las imágenes de las cartas se rendericen
+  const images = screen.getAllByRole('img');
+  expect(images.length).toBeGreaterThanOrEqual(1); // Ajusta el valor según la cantidad de cartas que esperas
 });
